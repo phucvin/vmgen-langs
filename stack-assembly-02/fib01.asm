c1617a12b03e5b2149c044373fce55c52cee5789
phucvin <phucvin@gmail.com>
2024-10-30 23:36:38 +0000

    stack-assembly-02 which translates code to register-assembly-01 underthehood

---
+++ stack-assembly-02/fib01.asm
@@ -0,0 +1,38 @@
+// compute fib(N) and print the result
+
+@main:
+  call_ret_addr @end
+  call_param #0 #6
+  call @fib
+@main_end:
+  puti
+  halt
+
+// params/locals #0=n
+@fib:
+  local_get #0
+  push #2
+// jump to @base_case if n < 2, fallthrough if not
+  jump_if_lt @fib_base_case 
+@fib_recursive_case:
+  call_ret_addr @fib_recursive_case_01
+  local_get #0
+  push #1
+  sub
+  call_param_tos #0
+  call @fib
+@fib_recursive_case_01:
+  call_ret_addr @fib_recursive_case_02
+  local_get #0
+  push #2
+  sub
+  call_param_tos #0
+  call @fib
+@fib_recursive_case_02:
+  add
+  return_param_tos
+  return
+@fib_base_case:
+  local_get #0
+  return_param #0
+  return
