e09991c87c6688e16b01ac38da82865b49cc28cc
phucvin <phucvin@gmail.com>
2024-10-31 23:39:08 +0000

    copy vm from learn-vmgen-01 and remove stuff that is unnecessary for stack bytecode

---
+++ stack-assembly-01/main.c
@@ -0,0 +1,81 @@
+#include <stdlib.h>
+#include "vm.h"
+#include "vm-gen.i"
+
+#define CODE_SIZE 128
+#define STACK_SIZE 128
+typedef long (*engine_t)(Inst *ip0, Cell *sp, char *fp);
+
+void genarg_i(Inst **vmcodepp, long i)
+{
+  vm_i2Cell(i, *((Cell *)*vmcodepp));
+  (*vmcodepp)++;
+}
+
+void printarg_i(long i)
+{
+  fprintf(vm_out, "%ld ", i);
+}
+
+void genarg_target(Inst **vmcodepp, Inst *target)
+{
+  vm_target2Cell(target, *((Cell *)*vmcodepp));
+  (*vmcodepp)++;
+}
+
+void printarg_target(Inst *target)
+{
+  fprintf(vm_out, "%p ", target);
+}
+
+void printarg_Cell(Cell i)
+{
+  fprintf(vm_out, "0x%lx ", i.i);
+}
+
+Label *vm_prim;
+Inst *vmcodep;
+FILE *vm_out;
+int vm_debug;
+
+int main(int argc, char **argv)
+{
+  Inst *vm_code = (Inst *)calloc(CODE_SIZE, sizeof(Inst));
+  Inst *start;
+  Cell *stack = (Cell *)calloc(STACK_SIZE, sizeof(Cell));
+  engine_t runvm;
+#ifdef VM_DEBUG
+  runvm = engine_debug;
+  vm_debug = 1;
+#else
+  runvm = engine;
+  vm_debug = 0;
+#endif
+
+  vmcodep = vm_code;
+  vm_out = stderr;
+  (void)runvm(NULL, NULL, NULL); /* initialize vm_prim */
+  init_peeptable();
+
+  start = vmcodep;
+  // Generate bytecode directly here for now
+  int code = 0;
+  if (code == 0) {
+    gen_end(&vmcodep);
+  }
+  vmcode_end = vmcodep;
+
+  printf("\nvm assembly:\n");
+  vm_disassemble(vm_code, vmcodep, vm_prim);
+
+  printf("\nvm run:\n");
+  long return_code = runvm(start, stack + STACK_SIZE - 1, NULL);
+  printf("%sreturn code: %ld\n", vm_debug ? "\n" : "", return_code);
+
+#ifdef VM_PROFILING
+  printf("\nvm profiling:\n");
+  vm_print_profile(vm_out);
+#endif
+
+  return 0;
+}
---
+++ stack-assembly-01/vm.h
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+typedef void *Label;
+
+typedef union Cell
+{
+  Label inst;
+  long i;
+  union Cell *target;
+} Cell, Inst;
+
+#define vm_Cell2i(_cell, _x) ((_x) = (_cell).i)
+#define vm_i2Cell(_x, _cell) ((_cell).i = (_x))
+#define vm_Cell2target(_cell,_x) ((_x)=(_cell).target)
+#define vm_target2Cell(_x,_cell) ((_cell).target=(_x))	
+#define vm_Cell2Cell(_x, _y) ((_y) = (_x))
+
+#define VM_IS_INST(_inst, n) ((_inst).inst == vm_prim[n])
+
+#define IMM_ARG(access, value) (access)
+
+extern Label *vm_prim;
+extern struct Peeptable_entry **peeptable;
+extern int vm_debug;
+extern FILE *vm_out;
+extern Inst *vmcodep;
+extern Inst *last_compiled;
+extern Inst *vmcode_end;
+extern int use_super;
+
+/* generic vmgen support functions (e.g., wrappers) */
+void gen_inst(Inst **vmcodepp, Label i);
+void init_peeptable(void);
+void vm_disassemble(Inst *ip, Inst *endp, Label prim[]);
+void vm_count_block(Inst *ip);
+struct block_count *block_insert(Inst *ip);
+void vm_print_profile(FILE *file);
+
+/* vm type-specific support functions */
+void genarg_i(Inst **vmcodepp, long i);
+void printarg_i(long i);
+void genarg_target(Inst **vmcodepp, Inst *target);
+void printarg_target(Inst *target);
+void printarg_Cell(Cell i);
+
+/* engine functions (type not fixed) */
+long engine(Inst *ip0, Cell *sp, char *fp);
+long engine_debug(Inst *ip0, Cell *sp, char *fp);
