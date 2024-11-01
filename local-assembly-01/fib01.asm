131329ac16a13eefa4b59243f977254f1f13417b
phucvin <phucvin@gmail.com>
2024-11-01 06:11:02 +0000

    use v prefix for locals (vars)

--- local-assembly-01/fib01.asm
+++ local-assembly-01/fib01.asm
@@ -12,19 +12,19 @@
   jump_if_lt r0 #2 @fib_end
 @fib_recursive_case:
   // save registers to locals since we're going to make calls
-  set l0 r0
-  set l1 rx
-  sub r0 l0 #1
+  set v0 r0
+  set v1 rx
+  sub r0 v0 #1
   set rx @fib_recursive_case_01
   jump @fib
 @fib_recursive_case_01:
-  set l2 r0
-  sub r0 l0 #2
+  set v2 r0
+  sub r0 v0 #2
   push @fib_recursive_case_02
   jump @fib
 @fib_recursive_case_02:
-  add r0 r0 l2
-  jump l1
+  add r0 r0 v2
+  jump v1
 @fib_base_case:
   // here we don't even needs to access locals since we didn't make any calls
   // registers are still the same with when we enter this function
