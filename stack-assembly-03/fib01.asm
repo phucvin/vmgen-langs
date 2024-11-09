5ce4c8c0bf7aaf99a7a85d4039636be822b0760a
phucvin <phucvin@gmail.com>
2024-11-09 22:04:26 +0000

    add ideal code for stack asm with faster access to anywhere in the stack by a delta

---
+++ stack-assembly-03/fib01.asm
@@ -0,0 +1,20 @@
+// compute fib(N) and print the result
+
+@main:
+  push #40
+  call @fib
+  halt ~1
+
+@fib:
+  // stack is now s( ret_addr n )
+  jump_lt @@base_case ~1 #2  // stack remains the same
+  sub ~1 #1  // s(.. n n-1 )
+  call @fib  // s(.. n fib(n-1) )
+  sub ~2 #2  // s(.. n fib(n-1) n-2 )
+  call @fib  // s(.. n fib(n-1) fib(n-2) )
+  add  // s( ret_addr n result )
+  // return using return address in ~3, return value in ~1
+  ret ~3 ~1 // s( result ), control is back to the caller
+@@base_case:
+  // now s( ret_address result_or_n )
+  ret ~2 ~1
