8b9a70416b55cba87b0b8f3d73f2f8a46a507caa
phucvin <phucvin@gmail.com>
2024-11-10 10:55:24 +0000

    minor fix

--- stack-assembly-04/examples/fib01.asm
+++ stack-assembly-04/examples/fib01.asm
@@ -7,11 +7,11 @@
 
 @fib:
   // stack is now s(| n )
-  jump_lt @@base_case |0 #2  // stack remains the same
+  jump_lt @@end |0 #2  // stack remains the same
   sub |0 #1  // s(| n n-1 )
   call @fib #1  // s(| n fib(n-1) )
   sub |0 #2  // s(| n fib(n-1) n-2 )
   call @fib #1  // s(| n fib(n-1) fib(n-2) )
   add  // s(| n result )
-@@base_case:
+@@end:
   ret  // s(.. n_or_result )
--- stack-assembly-04/examples/fib03.asm
+++ stack-assembly-04/examples/fib03.asm
@@ -7,11 +7,11 @@
 
 @fib:
   // stack is now s(| n )
-  jump_lt @@base_case |0 #2  // stack remains the same
+  jump_lt @@end |0 #2  // stack remains the same
   load |0, push #1, sub  // s(| n n-1 )
   call @fib #1  // s(| n fib(n-1) )
   load |0, push #2, sub  // s(| n fib(n-1) n-2 )
   call @fib #1  // s(| n fib(n-1) fib(n-2) )
   add  // s(| n result )
-@@base_case:
+@@end:
   ret  // s(.. n_or_result )
