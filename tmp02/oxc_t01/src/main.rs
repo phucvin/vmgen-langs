64567c359183e3b119f3aebaf10820bf7c1cc28b
phucvin <phucvin@gmail.com>
2024-11-18 22:35:30 +0000

    test oxc_parser

---
+++ tmp02/oxc_t01/src/main.rs
@@ -0,0 +1,30 @@
+use oxc_allocator::Allocator;
+use oxc_parser::{Parser, ParserReturn};
+use oxc_span::SourceType;
+
+fn main() {
+    let source_text = r"
+function fib(n: number) {
+    return n + 2;
+}
+console.log(fib(3));
+";
+
+    // Memory arena where AST nodes get stored
+    let allocator = Allocator::default();
+    // Infers TypeScript
+    let source_type = SourceType::from_path("t01.ts").unwrap();
+
+    let ParserReturn {
+        program,  // AST
+        errors,   // Syntax errors
+        panicked, // Parser encountered an error it couldn't recover from
+        ..
+    } = Parser::new(&allocator, source_text, source_type).parse();
+
+    println!("{:?}", program);
+    println!("{:?}", errors);
+    assert!(!panicked);
+    assert!(errors.is_empty());
+    assert!(!program.body.is_empty());
+}
