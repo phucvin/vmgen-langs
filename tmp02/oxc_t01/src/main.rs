8c6160667f6eb454da6b3e9529084844df57c8c2
phucvin <phucvin@gmail.com>
2024-11-18 22:54:30 +0000

    example visitor

--- tmp02/oxc_t01/src/main.rs
+++ tmp02/oxc_t01/src/main.rs
@@ -1,6 +1,12 @@
 use oxc_allocator::Allocator;
 use oxc_parser::{Parser, ParserReturn};
+use oxc_ast::{
+    ast::{Class, Function, TSImportType},
+    visit::walk,
+    Visit,
+};
 use oxc_span::SourceType;
+use oxc_syntax::scope::ScopeFlags;
 
 fn main() {
     let source_text = r"
@@ -25,4 +31,32 @@ console.log(fib(3));
     println!("Errors: {:?}", errors);
     assert!(!panicked);
     println!("AST:\n{:#?}", program);
+
+    let mut pass01 = Pass01::default();
+    pass01.visit_program(&program);
+    println!("{pass01:?}");
+}
+
+#[derive(Debug, Default)]
+struct Pass01 {
+    functions: usize,
+    classes: usize,
+    ts_import_types: usize,
+}
+
+impl<'a> Visit<'a> for Pass01 {
+    fn visit_function(&mut self, func: &Function<'a>, flags: ScopeFlags) {
+        self.functions += 1;
+        walk::walk_function(self, func, flags);
+    }
+
+    fn visit_class(&mut self, class: &Class<'a>) {
+        self.classes += 1;
+        walk::walk_class(self, class);
+    }
+
+    fn visit_ts_import_type(&mut self, ty: &TSImportType<'a>) {
+        self.ts_import_types += 1;
+        walk::walk_ts_import_type(self, ty);
+    }
 }
